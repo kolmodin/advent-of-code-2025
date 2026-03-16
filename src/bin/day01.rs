0e2e1c49b844726197d7738b3555d4b14c718714
Lennart Kolmodin <kolmodin@gmail.com>
2026-03-16 23:25:31 +0100

    Day 1, part 2.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,34 +1,43 @@
-use std::fs;
-
 use anyhow::{Context, Result, bail, ensure};
+use std::fs;
 
 fn parse_line(ln: &str) -> Result<i64> {
     ensure!(ln.len() > 1, "line too short: '{}'", ln);
     let n: i64 = ln[1..]
         .parse()
-        .with_context(|| format!("suffix not a number '{}'", ln))?;
+        .with_context(|| format!("suffix is not a number '{}'", ln))?;
     match ln.as_bytes()[0] {
         b'L' => Ok(n),
         b'R' => Ok(-n),
         _ => bail!("expected L or R when parsing '{}'", ln),
     }
 }
-
 fn main() -> Result<()> {
     let lines: Vec<i64> = fs::read_to_string("inputs/day01.txt")?
         .lines()
         .map(parse_line)
         .collect::<Result<Vec<_>>>()?;
 
-    let part1 = lines
-        .into_iter()
-        .scan(50, |state, x| {
-            *state = (*state + x) % 100;
-            Some(*state)
-        })
-        .filter(|x| *x == 0)
-        .count();
+    let mut part1 = 0;
+    let mut part2 = 0;
+
+    let _: i64 = lines.into_iter().fold(50, |state, dir| {
+        let rotated = state + dir;
+        if rotated.rem_euclid(100) == 0 {
+            part1 += 1;
+        }
+        part2 += (rotated / 100).abs();
+        if state * rotated < 0 || rotated == 0 {
+            part2 += 1;
+        }
+
+        rotated.rem_euclid(100)
+    });
 
     println!("Part 1: {}", part1);
+    assert_eq!(part1, 997);
+    println!("Part 2: {}", part2);
+    assert_eq!(part2, 5978);
+
     Ok(())
 }
