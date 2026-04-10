78db8b243c7108360f6895f24fcea470657a1b3f
Lennart Kolmodin <kolmodin@gmail.com>
2026-04-10 20:35:07 +0200

    Day 10, part 2.

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,8 +1,9 @@
 use std::str::FromStr;
 
-use anyhow::{Context, Result};
+use anyhow::{Context, Ok, Result, bail};
 
 use aoc_2025::input;
+use z3::{Optimize, ast::Int};
 
 #[derive(Debug)]
 struct Machine {
@@ -59,7 +60,7 @@ impl FromStr for Machine {
     }
 }
 
-fn min_pushed_buttons(machine: &Machine) -> Result<u16> {
+fn min_presses_to_match_lights(machine: &Machine) -> Result<u16> {
     let buttons = machine
         .buttons
         .iter()
@@ -82,16 +83,68 @@ fn min_pushed_buttons(machine: &Machine) -> Result<u16> {
         .context("no solution found")
 }
 
+fn min_presses_for_joltage(machine: &Machine) -> Result<u64> {
+    let opt = Optimize::new();
+
+    let button_ints = (0..machine.buttons.len())
+        .map(|i| Int::fresh_const(&format!("Button {}", i)))
+        .collect::<Vec<Int>>();
+
+    for b in &button_ints {
+        opt.assert(&b.ge(0));
+    }
+
+    // [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+    for (i, &target) in machine.joltage.iter().enumerate() {
+        // looking for buttons that can increment jolt 'i'
+        let sum = Int::add(
+            &machine
+                .buttons
+                .iter()
+                .zip(&button_ints)
+                .filter(|(b, _)| b.triggers.contains(&(i as u16)))
+                .map(|(_, int)| int.clone())
+                .collect::<Vec<Int>>(),
+        );
+        opt.assert(sum.eq(target));
+    }
+
+    let total_sum = Int::add(&button_ints);
+    opt.assert(&total_sum.gt(0));
+    opt.minimize(&total_sum);
+
+    if opt.check(&[]) == z3::SatResult::Sat {
+        let model = opt.get_model().context("failed to get model")?;
+        model
+            .eval(&total_sum, false)
+            .and_then(|v| v.as_u64())
+            .context("failed to eval and convert sum")
+    } else {
+        bail!("no solution found")
+    }
+}
+
 fn main() -> Result<()> {
     let input = input::read_input_day(10)?
         .lines()
         .map(Machine::from_str)
         .collect::<Result<Vec<Machine>>>()?;
 
-    let part1: u16 = input.iter().map(min_pushed_buttons).sum::<Result<u16>>()?;
+    let part1: u16 = input
+        .iter()
+        .map(min_presses_to_match_lights)
+        .sum::<Result<u16>>()?;
 
     println!("Part 1: {}", part1);
     assert_eq!(part1, 571);
+
+    let part2 = input
+        .iter()
+        .map(min_presses_for_joltage)
+        .sum::<Result<u64>>()?;
+
+    println!("Part 2: {}", part2);
+    assert_eq!(part2, 20869);
 
     Ok(())
 }
